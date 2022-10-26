f69ff1fade84f1b37e37ae09f0d6159dba07e481
Mansoor Bahramand <x@developerx.io>
2022-10-25 21:04:57 -0700

    added in prettier, eslint, and husky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import { defaultTheme } from 'tailwindcss'
+import { defaultTheme } from "tailwindcss"
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
